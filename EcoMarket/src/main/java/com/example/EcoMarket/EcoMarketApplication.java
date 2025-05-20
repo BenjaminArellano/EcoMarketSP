f088626d2e06a6121f13daa7139d0a4787398cd8
Bvnjvv <be.arellanor@duocuc.cl>
2025-05-20 18:35:23 -0400

    Conexion a base de datos correcta --  MAIN
    se conecto a SQL validando la creacion de todas las tablas definidias

--- EcoMarket/src/main/java/com/example/EcoMarket/EcoMarketApplication.java
+++ EcoMarket/src/main/java/com/example/EcoMarket/EcoMarketApplication.java
@@ -2,8 +2,32 @@ package com.example.EcoMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {
+	"Productos.repository",
+    "Usuarios.repository",
+    "Pedidos.repository",
+    "Productos.model",
+    "Pedidos.model",
+    "Usuarios.model",
+    "Productos.controller",
+    "Pedidos.controller",
+    "Usuarios.controller",
+    "Productos.service",
+    "Pedidos.service",
+    "Usuarios.service"})
+@EnableJpaRepositories(basePackages = {
+    "Productos.repository",
+    "Pedidos.repository",
+    "Usuarios.repository"
+})
+@EntityScan(basePackages = {
+    "Productos.model",
+    "Pedidos.model",
+    "Usuarios.model"
+})
 public class EcoMarketApplication {
 
 	public static void main(String[] args) {
