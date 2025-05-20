56b9ffc800420e56c46b628398f6365c4026c982
Francisco <fdhm3011@gmail.com>
2025-05-19 23:33:50 -0400

    creacion service -- Productos
    se crearon los service de las clases

---
+++ EcoMarket/src/main/java/Productos/service/ProductoService.java
@@ -0,0 +1,32 @@
+package Productos.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Productos.repository.ProductoRepository;
+import Productos.model.Producto;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    public Producto obtenerPorId(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    public Producto guardar(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public void eliminar(Long id) {
+        productoRepository.deleteById(id);
+    }
+}
